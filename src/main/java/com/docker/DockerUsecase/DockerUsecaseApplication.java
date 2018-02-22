b1bce8957b09a22d33a9c75ea6bb785f6768b0f4
Shankar K <shankar.1816@gmail.com>
2018-02-22 15:20:50 +0530

    Docker Use case

---
+++ src/main/java/com/docker/DockerUsecase/DockerUsecaseApplication.java
@@ -0,0 +1,12 @@
+package com.docker.DockerUsecase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerUsecaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerUsecaseApplication.class, args);
+	}
+}
